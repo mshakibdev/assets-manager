0c83c30280e1db2573ab432808a8f602c84cb6d8
mshakibdev <mshassan.dhk313@gmail.com>
2025-05-26 15:55:05 +0600

    first commit

---
+++ contentScript.js
@@ -0,0 +1,20 @@
+function getImages() {
+
+    // Get all image sources (absolute URLs)
+    const imgTags = document.querySelectorAll('img');
+    const images = [];
+    for (let img of imgTags) {
+        const src = img.src;
+        if (src) images.push(src);
+    }
+
+    return {images };
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'getImageData') {
+        const data = getImages();
+        sendResponse(data);
+    }
+    return true;
+});
